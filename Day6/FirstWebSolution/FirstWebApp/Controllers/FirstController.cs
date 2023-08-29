cdba143bfd78c32642ec43f053d59f99e897e230
Gayathri Mahadevan <gayat_19@yahoo.com>
2023-08-29 14:56:09 +0530

    Day6

---
+++ Day6/FirstWebSolution/FirstWebApp/Controllers/FirstController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Another()
+        {
+            return View();
+        }
+    }
+}
---
+++ Day6/FirstWebSolution/FirstWebApp/Controllers/PizzaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class PizzaController : Controller
+    {
+        List<string> pizzas = new List<string>
+        {
+            "ABC","XYZ","EFG"
+        };
+        public IActionResult Index()
+        {
+            ViewBag.PizzaName = "ABC Pizza";
+            ViewBag.PIzzaPrice = 200;
+            ViewBag.PizzaQuantity = 4;
+            return View();
+        }
+        public IActionResult List()
+        {
+            ViewBag.Pizzas = pizzas;
+            return View();
+        }
+    }
+}
